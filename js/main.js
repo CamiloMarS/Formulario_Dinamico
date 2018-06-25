cce436a1a5cfa7e3038fcb42b72fdfec32e86168
SaulRamirezObregon <bychawita@gmail.com>
2018-06-25 12:19:36 -0500

    main.js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,21 @@
+var username = document.getElementById("txtUsername");
 var email = document.getElementById("txtUseremail");
 var password = document.getElementById("txtUserpassword");
-var username = document.getElementById("txtUsername");
 var mensajeUsuario = document.getElementById("imgUsername");
 var mensajeCorreo = document.getElementById("imgEmail");
 var mensajePassword = document.getElementById("imgPassword");
 var music = new Audio("./media/shooting star.mp3");
 var rutaImg = "./media/img/";
+var boton = document.getElementById("btnIngresar");
+
+this.boton.disabled = true;
 
 function validarUserName()
 {
     var valor = this.username.value;
     var regEx = /^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{0,29}$/;
 
-    if(valor.search(regEx) == 0)
+    if(valor.search(regEx) == 0 )
     {
         this.mensajeUsuario.style.display = "block";
         this.mensajeUsuario.src = rutaImg + "checked.png";
@@ -69,22 +72,63 @@ function validarPassword()
     }
 
 }
-let btn_ingresar = document.querySelector("#btnIngresar");
 
-    if(btn_ingresar!==null){
-        btn_ingresar.addEventListener("click", function(e){
 
-            var username = validarUserName();
-            var email = validarEmail();
-            var password = validarPassword();
-
-             console.log(e.target);
-            if( username && email && password)
-            {
-                document.querySelector(".degragado").style.display="none";
-                document.querySelector(".login_form").style.display = "none";
-                document.querySelector(".divBienvenida").style.display = "block";
-            }
-
-        });
+username.addEventListener
+(
+    "keypress", function(e)
+    {
+        var validaUserName = validarUserName();
+        console.log("username"+validaUserName);
+    
     }
+);
+
+email.addEventListener
+(
+    "keypress", function(e)
+    {
+        var validaUserEmail = validarEmail();
+        console.log("email"+validaUserEmail);
+    
+    }
+);
+
+password.addEventListener
+(
+    "keypress", function(e)
+    {
+        var validaPassword = validarPassword();
+        console.log("password"+validaPassword);
+    }
+);
+
+/*
+function habilitarBotonMostrarBienvenida(user, email, pass)
+{
+    user = this.validaUserName;
+    email = this.validaUserEmail;
+    pass = this.validaPassword;
+    console.log("user "+user+" email "+email+" pass "+pass);
+    if(this.validaUserName && this.validaUserEmail && this.validaPassword)
+    {
+        this.boton.disabled = false;
+
+        if(this.boton !== null)
+        {
+            this.boton.addEventListener
+            (
+                "click", function(e)
+                {
+                    document.querySelector(".degragado").style.display="none";
+                    document.querySelector(".login_form").style.display = "none";
+                    document.querySelector(".divBienvenida").style.display = "block";    
+                }
+            );
+        }
+
+    }
+}
+
+habilitarBotonMostrarBienvenida();
+*/
